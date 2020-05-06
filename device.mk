682bc97cea0e46ebbf74b6822308ec2fb8535d5e
Bharat Dev Burman <bharat.singh7924@gmail.com>
2020-05-06 17:43:30 +0000

    fix typo, use correct dpi
    Change-Id: If83ad12838ac93bb9eaee0f5253cdb210b5fe741

--- device.mk
+++ device.mk
@@ -6,11 +6,11 @@ $(call inherit-product, vendor/samsung/a30/a30-vendor.mk)
 ### DALVIK
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
-DEVICE_PATH := device/samsung/beyond0lte
+DEVICE_PATH := device/samsung/a30/
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := 411dpi
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 
 DEVICE_PACKAGE_OVERLAYS += \
